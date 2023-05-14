f30f6adc4be41cb64204fd82d1763a6d586271ee
chagins <chagin_yav@hotmail.com>
2023-05-14 17:12:13 +0500

    feat: add heading in TasksListPage

--- TodoAPP/src/pages/TasksListPage/ui/TasksListPage.tsx
+++ TodoAPP/src/pages/TasksListPage/ui/TasksListPage.tsx
@@ -1,6 +1,15 @@
 import React from 'react';
+import { Typography } from '@mui/material';
+import Link from '@mui/material/Link';
 import { TasksList } from 'widgets/TasksList';
 
 export const TasksListPage = () => {
-  return <TasksList />;
+  return (
+    <>
+      <Typography mt={2} variant="h1" align="center">
+        Tasks list with <Link href="https://feature-sliced.design/ru/">feature sliced design</Link>{' '}
+      </Typography>
+      <TasksList />
+    </>
+  );
 };
